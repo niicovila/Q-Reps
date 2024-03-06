3facfabbe14754c25f07468623832d5c3a59e52c
niicovila <nicolas.vila01@estudiant.upf.edu>
2024-03-06 11:35:37 +0100

    atari

--- main/qreps/cleanrl/qreps_v2.py
+++ main/qreps/cleanrl/qreps_v2.py
@@ -1,4 +1,4 @@
-# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/sac/#sac_ataripy
+# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/dqn/#dqnpy
 import os
 import random
 import time
@@ -11,16 +11,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import tyro
-from stable_baselines3.common.atari_wrappers import (
-    ClipRewardEnv,
-    EpisodicLifeEnv,
-    FireResetEnv,
-    MaxAndSkipEnv,
-    NoopResetEnv,
-)
 from stable_baselines3.common.buffers import ReplayBuffer
-from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
+from torch.distributions import Categorical
+from losses import empirical_logistic_bellman, log_gumbel
 
 
 @dataclass
@@ -35,18 +29,27 @@ class Args:
     """if toggled, cuda will be enabled by default"""
     track: bool = False
     """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
+    wandb_project_name: str = "SAC_Discrete"
     """the wandb's project name"""
     wandb_entity: str = None
     """the entity (team) of wandb's project"""
     capture_video: bool = False
     """whether to capture videos of the agent performances (check out `videos` folder)"""
 
+    save_model: bool = False
+    """whether to save model into the `runs/{run_name}` folder"""
+    upload_model: bool = False
+    """whether to upload the saved model to huggingface"""
+    hf_entity: str = ""
+    """the user or org name of the model repository from the Hugging Face Hub"""
+
     # Algorithm specific arguments
-    env_id: str = "BeamRiderNoFrameskip-v4"
+    env_id: str = "LunarLander-v2"
     """the id of the environment"""
-    total_timesteps: int = 5000000
+    total_timesteps: int = 500000
     """total timesteps of the experiments"""
+    num_envs: int = 1
+    """the number of parallel game environments"""
     buffer_size: int = int(1e6)
     """the replay memory buffer size"""  # smaller than in original paper but evaluation is done only for 100k steps anyway
     gamma: float = 0.99
@@ -63,17 +66,14 @@ class Args:
     """the learning rate of the Q network network optimizer"""
     update_frequency: int = 4
     """the frequency of training updates"""
-    target_network_frequency: int = 8000
+    target_network_frequency: int = 32
     """the frequency of updates for the target networks"""
-    alpha: float = 0.2
+    alpha: float = 2
     """Entropy regularization coefficient."""
-    eta: float = None
-    """KL regularization coefficient."""
     autotune: bool = True
     """automatic tuning of the entropy coefficient"""
     target_entropy_scale: float = 0.89
     """coefficient for scaling the autotune entropy target"""
-
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
@@ -83,93 +83,60 @@ def make_env(env_id, seed, idx, capture_video, run_name):
         else:
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
-
-        env = NoopResetEnv(env, noop_max=30)
-        env = MaxAndSkipEnv(env, skip=4)
-        env = EpisodicLifeEnv(env)
-        if "FIRE" in env.unwrapped.get_action_meanings():
-            env = FireResetEnv(env)
-        env = ClipRewardEnv(env)
-        env = gym.wrappers.ResizeObservation(env, (84, 84))
-        env = gym.wrappers.GrayScaleObservation(env)
-        env = gym.wrappers.FrameStack(env, 4)
-
         env.action_space.seed(seed)
+
         return env
 
     return thunk
 
-
-def layer_init(layer, bias_const=0.0):
-    nn.init.kaiming_normal_(layer.weight)
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
-
 # ALGO LOGIC: initialize agent here:
-# NOTE: Sharing a CNN encoder between Actor and Critics is not recommended for SAC without stopping actor gradients
-# See the SAC+AE paper https://arxiv.org/abs/1910.01741 for more info
-# TL;DR The actor's gradients mess up the representation when using a joint encoder
-class SoftQNetwork(nn.Module):
-    def __init__(self, envs):
+class QNetwork(nn.Module):
+    def __init__(self, env):
         super().__init__()
-        obs_shape = envs.single_observation_space.shape
-        self.conv = nn.Sequential(
-            layer_init(nn.Conv2d(obs_shape[0], 32, kernel_size=8, stride=4)),
+        self.network = nn.Sequential(
+            layer_init(nn.Linear(np.array(env.single_observation_space.shape).prod(), 120)),
             nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2)),
+            nn.Linear(120, 84),
             nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1)),
-            nn.Flatten(),
+            layer_init(nn.Linear(84, env.single_action_space.n)),
         )
 
-        with torch.inference_mode():
-            output_dim = self.conv(torch.zeros(1, *obs_shape)).shape[1]
-
-        self.fc1 = layer_init(nn.Linear(output_dim, 512))
-        self.fc_q = layer_init(nn.Linear(512, envs.single_action_space.n))
-
     def forward(self, x):
-        x = F.relu(self.conv(x / 255.0))
-        x = F.relu(self.fc1(x))
-        q_vals = self.fc_q(x)
-        return q_vals
+        return self.network(x)
 
-
-class Actor(nn.Module):
-    def __init__(self, envs):
+class QREPSPolicy(nn.Module):
+    def __init__(self, env):
         super().__init__()
-        obs_shape = envs.single_observation_space.shape
-        self.conv = nn.Sequential(
-            layer_init(nn.Conv2d(obs_shape[0], 32, kernel_size=8, stride=4)),
+        self.actor = nn.Sequential(
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 120)),
             nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2)),
+            nn.Linear(120, 84),
             nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1)),
-            nn.Flatten(),
+            layer_init(nn.Linear(84, envs.single_action_space.n)),
         )
 
-        with torch.inference_mode():
-            output_dim = self.conv(torch.zeros(1, *obs_shape)).shape[1]
-
-        self.fc1 = layer_init(nn.Linear(output_dim, 512))
-        self.fc_logits = layer_init(nn.Linear(512, envs.single_action_space.n))
-
     def forward(self, x):
-        x = F.relu(self.conv(x))
-        x = F.relu(self.fc1(x))
-        logits = self.fc_logits(x)
+        return self.actor(x)
 
-        return logits
-
-    def get_action(self, x):
-        logits = self(x / 255.0)
+    def get_action(self, x, action=None):
+        logits = self(x)
         policy_dist = Categorical(logits=logits)
-        action = policy_dist.sample()
-        # Action probabilities for calculating the adapted soft-Q loss
+        if action is None: action = policy_dist.sample()
+
         action_probs = policy_dist.probs
         log_prob = F.log_softmax(logits, dim=1)
-        return action, log_prob, action_probs
+        action_log_prob = policy_dist.log_prob(action)
+        return action, action_log_prob, log_prob, action_probs
+    
+
+def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
+    slope = (end_e - start_e) / duration
+    return max(slope * t + start_e, end_e)
 
 
 if __name__ == "__main__":
@@ -179,11 +146,11 @@ if __name__ == "__main__":
         raise ValueError(
             """Ongoing migration: run the following command to install the new dependencies:
 
-poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-license]==0.28.1"  "ale-py==0.8.1" 
+poetry run pip install "stable_baselines3==2.0.0a1"
 """
         )
     args = tyro.cli(Args)
-    if args.eta is None: args.eta = args.alpha
+    assert args.num_envs == 1, "vectorized envs are not supported at the moment"
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     if args.track:
         import wandb
@@ -212,21 +179,24 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
     # env setup
-    envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed, 0, args.capture_video, run_name)])
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.env_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
+    )
     assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
-    actor = Actor(envs).to(device)
-    qf1 = SoftQNetwork(envs).to(device)
-    qf2 = SoftQNetwork(envs).to(device)
-    qf1_target = SoftQNetwork(envs).to(device)
-    qf2_target = SoftQNetwork(envs).to(device)
+    actor = QREPSPolicy(envs).to(device)
+
+    qf1 = QNetwork(envs).to(device)
+    qf2 = QNetwork(envs).to(device)
+    qf1_target = QNetwork(envs).to(device)
+    qf2_target = QNetwork(envs).to(device)
+
     qf1_target.load_state_dict(qf1.state_dict())
     qf2_target.load_state_dict(qf2.state_dict())
-    # TRY NOT TO MODIFY: eps=1e-4 increases numerical stability
+
     q_optimizer = optim.Adam(list(qf1.parameters()) + list(qf2.parameters()), lr=args.q_lr, eps=1e-4)
     actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.policy_lr, eps=1e-4)
 
-    # Automatic entropy tuning
     if args.autotune:
         target_entropy = -args.target_entropy_scale * torch.log(1 / torch.tensor(envs.single_action_space.n))
         log_alpha = torch.zeros(1, requires_grad=True, device=device)
@@ -247,11 +217,10 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
     # TRY NOT TO MODIFY: start the game
     obs, _ = envs.reset(seed=args.seed)
     for global_step in range(args.total_timesteps):
-        # ALGO LOGIC: put action logic here
         if global_step < args.learning_starts:
             actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
         else:
-            actions, _, _ = actor.get_action(torch.Tensor(obs).to(device))
+            actions, _, _, _ = actor.get_action(torch.Tensor(obs).to(device))
             actions = actions.detach().cpu().numpy()
 
         # TRY NOT TO MODIFY: execute the game and log data.
@@ -273,6 +242,7 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
         for idx, trunc in enumerate(truncations):
             if trunc:
                 real_next_obs[idx] = infos["final_observation"][idx]
+
         rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
 
         # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
@@ -282,40 +252,51 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
         if global_step > args.learning_starts:
             if global_step % args.update_frequency == 0:
                 data = rb.sample(args.batch_size)
-                # CRITIC training
                 with torch.no_grad():
-                    _, next_state_log_pi, next_state_action_probs = actor.get_action(data.next_observations)
+                    _, next_state_action_log_pi, next_state_log_pi, next_state_action_probs = actor.get_action(data.next_observations)
                     qf1_next_target = qf1_target(data.next_observations)
                     qf2_next_target = qf2_target(data.next_observations)
+
                     # we can use the action probabilities instead of MC sampling to estimate the expectation
                     min_qf_next_target = next_state_action_probs * (
                         torch.min(qf1_next_target, qf2_next_target) - alpha * next_state_log_pi
                     )
-                    # adapt Q-target for discrete Q-function
-                    # min_qf_next_target = min_qf_next_target.sum(dim=1)
                     
-                    min_qf_next_target = args.eta * torch.logsumexp(min_qf_next_target / args.eta, dim=1)
-                    next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (min_qf_next_target)
+                    min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
+                    value_target = alpha * torch.logsumexp(min_qf_next_target / alpha, dim=1)
 
-                # use Q-values only for the taken actions
+                    # adapt Q-target for discrete Q-function
+                    min_qf_next_target = min_qf_next_target.sum(dim=1)
+                    next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (value_target)
+
                 qf1_values = qf1(data.observations)
                 qf2_values = qf2(data.observations)
                 qf1_a_values = qf1_values.gather(1, data.actions.long()).view(-1)
                 qf2_a_values = qf2_values.gather(1, data.actions.long()).view(-1)
-                qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-                qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-                qf_loss = qf1_loss + qf2_loss
 
+                values_qf1 = alpha * torch.logsumexp(qf1_values / alpha, dim=1).detach()
+                values_qf2 = alpha * torch.logsumexp(qf2_values / alpha, dim=1).detach()
+
+                qf1_loss = log_gumbel(qf1_a_values, next_q_value, alpha, values_qf1, args.gamma)
+                qf2_loss = log_gumbel(qf2_a_values, next_q_value, alpha, values_qf2, args.gamma) 
+                
+                # qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+                # qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+
+                qf_loss = qf1_loss + qf2_loss
+                
+                # optimize the model
                 q_optimizer.zero_grad()
                 qf_loss.backward()
                 q_optimizer.step()
 
                 # ACTOR training
-                _, log_pi, action_probs = actor.get_action(data.observations)
+                _, action_log_pi, log_pi, action_probs = actor.get_action(data.observations)
                 with torch.no_grad():
                     qf1_values = qf1(data.observations)
                     qf2_values = qf2(data.observations)
                     min_qf_values = torch.min(qf1_values, qf2_values)
+
                 # no need for reparameterization, the expectation can be calculated for discrete actions
                 actor_loss = (action_probs * ((alpha * log_pi) - min_qf_values)).mean()
 
@@ -332,12 +313,13 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
                     a_optimizer.step()
                     alpha = log_alpha.exp().item()
 
-            # update the target networks
+            # update target network
             if global_step % args.target_network_frequency == 0:
                 for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
                     target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
                 for param, target_param in zip(qf2.parameters(), qf2_target.parameters()):
                     target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+
 
             if global_step % 100 == 0:
                 writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
